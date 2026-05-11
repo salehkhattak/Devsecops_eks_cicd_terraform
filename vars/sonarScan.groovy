78a5025a3ed057898b379396b5cd4d0c63c6e76b
saleh khattak <salehktk005@gmail.com>
2026-05-12 01:33:53 +0500

    jenkinsfile updated

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -1,12 +1,20 @@
 def call() {
     echo "Running SonarQube analysis..."
 
+    // Pull the scanner image first to give clear feedback if pull fails
+    sh "docker pull sonarsource/sonar-scanner-cli"
+
+    // Run the scanner on the same Docker network as the SonarQube server
+    // so the hostname 'sonarqube' resolves correctly.
     sh """
         docker run --rm \
+        --network devsecops-eks-proj_three-tier \
         -e SONAR_HOST_URL="http://sonarqube:9000" \
         -e SONAR_LOGIN="sqa_3424188e008857466d964e870cfa90e7d19fd959" \
         -v \$PWD:/usr/src \
-        sonarsource/sonar-scanner-cli
+        sonarsource/sonar-scanner-cli \
+        -Dsonar.projectKey=flask-devsecops \
+        -Dsonar.sources=/usr/src
     """
 
     echo "SonarQube analysis complete!"
