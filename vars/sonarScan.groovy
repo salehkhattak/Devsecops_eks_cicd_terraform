79dd55979b2379ef9470a68b2dab65828e3555a9
saleh khattak <salehktk005@gmail.com>
2026-05-12 01:02:25 +0500

    jenkinsfile updated

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -4,7 +4,7 @@ def call() {
     sh """
         docker run --rm \
         -e SONAR_HOST_URL="http://sonarqube:9000" \
-        -e SONAR_LOGIN="YOUR_SONAR_TOKEN" \
+        -e SONAR_LOGIN="sqa_3424188e008857466d964e870cfa90e7d19fd959" \
         -v \$PWD:/usr/src \
         sonarsource/sonar-scanner-cli
     """
